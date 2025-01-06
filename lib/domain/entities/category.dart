f83c2a6af1157eb288c4f96822c0e0bbf2892ec8
JC <thawedbuffalo@gmail.com>
2025-01-06 07:34:13 -0500

    added freezed package and category entity

---
+++ lib/domain/entities/category.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
+
+part 'category.freezed.dart';
+part 'category.g.dart';
+
+@freezed
+class Category with _$Category {
+  Category._();
+
+  @HiveType(typeId: 0)
+  factory Category({required int id, required String name}) = _Category;
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      _$CategoryFromJson(json);
+}
