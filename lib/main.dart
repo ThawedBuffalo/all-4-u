53b50ce4a5231ab5e42c1a069e0f58a56fc1ba91
JC <thawedbuffalo@gmail.com>
2025-01-08 11:09:05 -0500

    removed issues from using toast notifications, corrected Hive adapter build issues by moving to dev depends

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,9 @@ import 'package:all_4_u/domain/entities/category.dart';
 import 'package:all_4_u/domain/entities/item.dart';
 import 'package:all_4_u/domain/entities/person.dart';
 import 'package:all_4_u/presentation/pages/home/home_page.dart';
-import 'package:flash/flash_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:all_4_u/themes/buffalo-sabres-light-theme.dart';
 import 'package:all_4_u/core/logging/custom_logger.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:path_provider/path_provider.dart';
-import 'core/logging/logger_enums.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -38,32 +33,17 @@ init() async {
   Hive.registerAdapter(ItemAdapter());
 
   CustomLogger.loggerNoStack.i('application starting...');
-  runApp(const App());
+  runApp(App());
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _AppState();
-  }
-}
-
-class _AppState extends State<App> {
+class App extends StatelessWidget {
   final navigatorKey = GlobalKey<NavigatorState>();
+
+  App({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, _) {
-        var child = _!;
-        child = Toast(
-          navigatorKey: navigatorKey,
-          child: child,
-        );
-        return child;
-      },
       title: 'Placeholder Page',
       //theme: buffaloSabresLightTheme,
       home: const HomePage(),
