7f0ec3041f9bb2185db117cb617e8bd0c17af21e
JC Reid <thawedbuffalo@gmail.com>
2025-01-14 21:53:54 -0500

    added create category usecase, DB failure test

--- test/domain/usecases/create_category_usecase_test.dart
+++ test/domain/usecases/create_category_usecase_test.dart
@@ -1,3 +1,5 @@
+import 'package:all_4_u/core/error/error_messages.dart';
+import 'package:all_4_u/core/error/failure.dart';
 import 'package:all_4_u/data/repositories/category_repository.dart';
 import 'package:all_4_u/domain/entities/category_entity.dart';
 import 'package:all_4_u/domain/entities/category_entity_id.dart';
@@ -12,19 +14,30 @@ void main() {
   final CategoryRepository repository = MockCategoryRepository();
   final CreateCategoryUseCase usecase = CreateCategoryUseCase(repository);
 
-  final testName = 'useCaseTestName';
+  final successTestName = 'successUseCaseTestName';
+  final failTestName = 'failedUseCaseTestName';
   final testCategoryEntityId = CategoryEntityId(id: 71114);
-  final testCategoryEntity = CategoryEntity(id: testCategoryEntityId, name: testName);
-
-  setUp(() {
-    when(repository.insertCategory(testName)).thenAnswer((_) async => Right(testCategoryEntity));
-  });
+  final testCategoryEntity = CategoryEntity(id: testCategoryEntityId, name: successTestName);
 
 
-  test('should return CategoryEntity', () async {
-    final result = await usecase.call(Params(categoryName: testName));
+    setUp(() {
+      when(repository.insertCategory(successTestName)).thenAnswer((_) async =>
+          Right(testCategoryEntity));
+      when(repository.insertCategory(failTestName)).thenAnswer((_) async =>
+          Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
+    });
 
-    verify(repository.insertCategory(testName)).called(1);
-    expect(result, equals(Right(testCategoryEntity)));
+    test('should return CategoryEntity', () async {
+        final result = await usecase.call(Params(categoryName: successTestName));
+
+        verify(repository.insertCategory(successTestName)).called(1);
+        expect(result, equals(Right(testCategoryEntity)));
+    });
+
+  test('should return DB Failure', () async {
+    final result = await usecase.call(Params(categoryName: failTestName));
+
+    verify(repository.insertCategory(failTestName)).called(1);
+    expect(result, equals(Left(DBFailure(errorMessage: DB_INSERT_FAILURE))));
   });
 }
