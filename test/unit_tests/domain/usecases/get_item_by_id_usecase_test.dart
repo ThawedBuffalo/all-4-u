65300458fce90323a3cd9f58a0ea75ed63822661
JC <thawedbuffalo@gmail.com>
2025-03-21 06:36:10 -0400

    testing use cases: delete all items use cases completed

--- test/unit_tests/domain/usecases/get_item_by_id_usecase_test.dart
+++ test/unit_tests/domain/usecases/get_item_by_id_usecase_test.dart
@@ -1,53 +1,59 @@
-// import 'package:all_4_u/core/error/error_messages.dart';
-// import 'package:all_4_u/core/error/failure.dart';
-// import 'package:all_4_u/data/repositories/category_repository.dart';
-// import 'package:all_4_u/data/repositories/item_repository.dart';
-// import 'package:all_4_u/domain/entities/category_entity.dart';
-// import 'package:all_4_u/domain/entities/item_entity.dart';
-// import 'package:all_4_u/domain/usecases/get_category_by_id_usecase.dart';
-// import 'package:all_4_u/domain/usecases/get_item_by_id_usecase.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-// import 'package:dartz/dartz.dart';
-//
-// import '../../mock/domain/repository/category_repository_mock.mocks.dart';
-// import '../../mock/domain/repository/item_repository_mock.mocks.dart';
-//
-// void main() {
-//   final ItemRepository repository = MockItemRepository();
-//   final GetItemByIdUseCase usecase = GetItemByIdUseCase(repository);
-//
-//   final String successItemName = 'successItemName';
-//   final String failItemName = 'failItemName';
-//   final String testDescription = 'testDescription';
-//   final int testId = 17;
-//   final List<int> testCategoryIds = [7, 11, 14];
-//   final List<int> testPersonIds = [2, 3, 13, 17, 86];
-//   final ItemEntity successTestItemEntity = ItemEntity(
-//       id: testId,
-//       name: successItemName,
-//       description: testDescription,
-//       categoryIdList: testCategoryIds,
-//       personIdList: testPersonIds);
-//
-//   setUp(() {
-//     when(repository.getItemById(testId))
-//         .thenAnswer((_) async => Right(successTestItemEntity));
-//     when(repository.getCategoryById(failCategoryId)).thenAnswer(
-//         (_) async => Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
-//   });
-//
-//   test('should return CategoryEntity', () async {
-//     final result = await usecase.call(Params(categoryId: successCategoryId));
-//
-//     verify(repository.getCategoryById(successCategoryId)).called(1);
-//     expect(result, equals(Right(testCategoryEntity)));
-//   });
-//
-//   test('should return DB Failure', () async {
-//     final result = await usecase.call(Params(categoryId: failCategoryId));
-//
-//     verify(repository.getCategoryById(failCategoryId)).called(1);
-//     expect(result, equals(Left(DBFailure(errorMessage: DB_INSERT_FAILURE))));
-//   });
-// }
+import 'package:all_4_u/core/error/error_messages.dart';
+import 'package:all_4_u/core/error/failure.dart';
+import 'package:all_4_u/core/logging/custom_logger.dart';
+import 'package:all_4_u/data/dtos/item_dto.dart';
+import 'package:all_4_u/data/repositories/category_repository.dart';
+import 'package:all_4_u/data/repositories/item_repository.dart';
+import 'package:all_4_u/domain/entities/category_entity.dart';
+import 'package:all_4_u/domain/entities/item_entity.dart';
+import 'package:all_4_u/domain/usecases/get_category_by_id_usecase.dart';
+import 'package:all_4_u/domain/usecases/get_item_by_id_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../helpers/Fake_item_dto_factory.dart';
+import 'get_item_by_id_usecase_test.mocks.dart';
+
+@GenerateMocks([ItemRepository])
+void main() {
+  final ItemRepository repository = MockItemRepository();
+  final GetItemByIdUseCase usecase = GetItemByIdUseCase(repository);
+
+  FakeItemDTOFactory itemDTOFactory = FakeItemDTOFactory();
+  ItemDTO testSuccessItemDTO = itemDTOFactory.generateFake();
+  ItemEntity testSuccessItemEntity =
+      itemDTOFactory.generateFakeEntity(testSuccessItemDTO);
+  ItemDTO testFailureItemDTO = itemDTOFactory.generateFake();
+  ItemEntity testFailureItemEntity =
+      itemDTOFactory.generateFakeEntity(testFailureItemDTO);
+
+  group('-> getItemById() <-', () {
+    test('should return ItemEntity', () async {
+      CustomLogger.loggerNoStack.i('-> getItemById() <- test starting...');
+
+      when(repository.getItemById(id: testSuccessItemEntity.id))
+          .thenAnswer((_) async => Right(testSuccessItemEntity));
+
+      final result = await usecase
+          .call(GetItemByIdParams(itemId: testSuccessItemEntity.id));
+
+      verify(repository.getItemById(id: testSuccessItemEntity.id)).called(1);
+      expect(result, equals(Right(testSuccessItemEntity)));
+    });
+
+    test('should return DB Failure', () async {
+      CustomLogger.loggerNoStack.i('-> getItemById() <- test starting...');
+
+      when(repository.getItemById(id: testFailureItemEntity.id)).thenAnswer(
+          (_) async => Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
+
+      final result = await usecase
+          .call(GetItemByIdParams(itemId: testFailureItemEntity.id));
+
+      verify(repository.getItemById(id: testFailureItemEntity.id)).called(1);
+      expect(result, equals(Left(DBFailure(errorMessage: DB_INSERT_FAILURE))));
+    });
+  });
+}
