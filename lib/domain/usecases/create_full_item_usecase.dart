cd7dd592e11f551b809bd460ea5d458f63918024
JC <thawedbuffalo@gmail.com>
2025-03-21 08:22:18 -0400

    testing use cases: create full items use cases completed

--- lib/domain/usecases/create_full_item_usecase.dart
+++ lib/domain/usecases/create_full_item_usecase.dart
@@ -1,40 +1,45 @@
-// import 'package:all_4_u/core/error/failure.dart';
-// import 'package:all_4_u/core/usecases/usecase.dart';
-// import 'package:dartz/dartz.dart';
-// import 'package:equatable/equatable.dart';
-//
-// import '../../core/error/error_messages.dart';
-// import '../repositories/item_repository_intf.dart';
-//
-// class CreateFullItemUseCase implements UseCase<void, CreateFullItemParams> {
-//   final ItemRepositoryInterface repository;
-//
-//   const CreateFullItemUseCase(this.repository);
-//
-//   @override
-//   Future<Either<Failure, dynamic>> call(CreateFullItemParams params) async {
-//     final result = await repository.insertFullItem(
-//         params.name, params.description, params.categoryIds, params.personIds);
-//     if (result.isLeft()) {
-//       return Future.value(Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
-//     }
-//     return Future.value(result);
-//   }
-// }
-//
-// class CreateFullItemParams extends Equatable {
-//   final String name;
-//   final String description;
-//   final List<int> categoryIds;
-//   final List<int> personIds;
-//
-//   const CreateFullItemParams(
-//       {required this.name,
-//       required this.description,
-//       required this.categoryIds,
-//       required this.personIds})
-//       : super();
-//
-//   @override
-//   List<Object?> get props => [name, description, categoryIds, personIds];
-// }
+import 'package:all_4_u/core/error/failure.dart';
+import 'package:all_4_u/core/usecases/usecase.dart';
+import 'package:all_4_u/domain/entities/category_entity_list.dart';
+import 'package:all_4_u/domain/entities/person_entity_list.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../core/error/error_messages.dart';
+import '../repositories/item_repository_intf.dart';
+
+class CreateFullItemUseCase implements UseCase<void, CreateFullItemParams> {
+  final ItemRepositoryInterface repository;
+
+  const CreateFullItemUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, dynamic>> call(CreateFullItemParams params) async {
+    final result = await repository.createFullItem(
+        name: params.name,
+        description: params.description,
+        categoryEntityList: params.categories,
+        personEntityList: params.people);
+    if (result.isLeft()) {
+      return Future.value(Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
+    }
+    return Future.value(result);
+  }
+}
+
+class CreateFullItemParams extends Equatable {
+  final String name;
+  final String? description;
+  final CategoryEntityList? categories;
+  final PersonEntityList? people;
+
+  const CreateFullItemParams(
+      {required this.name,
+      required this.description,
+      required this.categories,
+      required this.people})
+      : super();
+
+  @override
+  List<Object?> get props => [name, description, categories, people];
+}
--- test/unit_tests/domain/usecases/create_full_item_usecase_test.dart
+++ test/unit_tests/domain/usecases/create_full_item_usecase_test.dart
@@ -1,66 +1,87 @@
-// import 'package:all_4_u/core/error/error_messages.dart';
-// import 'package:all_4_u/core/error/failure.dart';
-// import 'package:all_4_u/data/repositories/item_repository.dart';
-// import 'package:all_4_u/domain/entities/item_entity.dart';
-// import 'package:all_4_u/domain/usecases/create_category_usecase.dart';
-// import 'package:all_4_u/domain/usecases/create_full_item_usecase.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-// import 'package:dartz/dartz.dart';
-//
-// import '../../mock/domain/repository/item_repository_mock.mocks.dart';
-//
-// void main() {
-//   final ItemRepository repository = MockItemRepository();
-//   final CreateFullItemUseCase usecase = CreateFullItemUseCase(repository);
-//
-//   // test data
-//   final String successItemName = 'successItemName';
-//   final String failItemName = 'failItemName';
-//   final String testDescription = 'testDescription';
-//   final int testId = 17;
-//   final List<int> testCategoryIds = [7, 11, 14];
-//   final List<int> testPersonIds = [2, 3, 13, 17, 86];
-//   final ItemEntity successTestItemEntity = ItemEntity(
-//       id: testId,
-//       name: successItemName,
-//       description: testDescription,
-//       categoryIdList: testCategoryIds,
-//       personIdList: testPersonIds);
-//
-//   setUp(() {
-//     when(repository.insertFullItem(
-//             successItemName, testDescription, testCategoryIds, testPersonIds))
-//         .thenAnswer((_) async => Right(successTestItemEntity));
-//     when(repository.insertFullItem(
-//             failItemName, testDescription, testCategoryIds, testPersonIds))
-//         .thenAnswer(
-//             (_) async => Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
-//   });
-//
-//   test('should return ItemEntity', () async {
-//     final result = await usecase.call(CreateFullItemParams(
-//         name: successItemName,
-//         description: testDescription,
-//         categoryIds: testCategoryIds,
-//         personIds: testPersonIds));
-//
-//     verify(repository.insertFullItem(
-//             successItemName, testDescription, testCategoryIds, testPersonIds))
-//         .called(1);
-//     expect(result, equals(Right(successTestItemEntity)));
-//   });
-//
-//   test('should return DB Failure', () async {
-//     final result = await usecase.call(CreateFullItemParams(
-//         name: failItemName,
-//         description: testDescription,
-//         categoryIds: testCategoryIds,
-//         personIds: testPersonIds));
-//
-//     verify(repository.insertFullItem(
-//             failItemName, testDescription, testCategoryIds, testPersonIds))
-//         .called(1);
-//     expect(result, equals(Left(DBFailure(errorMessage: DB_INSERT_FAILURE))));
-//   });
-// }
+import 'package:all_4_u/core/error/error_messages.dart';
+import 'package:all_4_u/core/error/failure.dart';
+import 'package:all_4_u/core/logging/custom_logger.dart';
+import 'package:all_4_u/data/dtos/category_dto.dart';
+import 'package:all_4_u/data/dtos/item_dto.dart';
+import 'package:all_4_u/data/dtos/person_dto.dart';
+import 'package:all_4_u/data/repositories/item_repository.dart';
+import 'package:all_4_u/domain/entities/category_entity.dart';
+import 'package:all_4_u/domain/entities/category_entity_list.dart';
+import 'package:all_4_u/domain/entities/item_entity.dart';
+import 'package:all_4_u/domain/entities/item_entity_list.dart';
+import 'package:all_4_u/domain/entities/person_entity_list.dart';
+import 'package:all_4_u/domain/usecases/create_category_usecase.dart';
+import 'package:all_4_u/domain/usecases/create_full_item_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../helpers/Fake_category_dto_factory.dart';
+import '../../../helpers/Fake_item_dto_factory.dart';
+import '../../../helpers/Fake_person_dto_factory.dart';
+
+import 'create_full_item_usecase_test.mocks.dart';
+
+@GenerateMocks([ItemRepository])
+void main() {
+  final ItemRepository repository = MockItemRepository();
+  final CreateFullItemUseCase usecase = CreateFullItemUseCase(repository);
+
+  FakeItemDTOFactory itemDTOFactory = FakeItemDTOFactory();
+  ItemDTO testItemDTO = itemDTOFactory.generateFake();
+  ItemEntity testItemEntity = itemDTOFactory.generateFakeEntity(testItemDTO);
+
+  group('-> createFullItem() <-', () {
+    test('should return ItemEntity', () async {
+      CustomLogger.loggerNoStack.i('-> createFullItem() <- test starting...');
+
+      when(repository.createFullItem(
+              name: testItemEntity.name,
+              description: testItemEntity.description,
+              categoryEntityList: testItemEntity.categoryList,
+              personEntityList: testItemEntity.personList))
+          .thenAnswer((_) async => Right(testItemEntity.id));
+
+      final result = await usecase.call(CreateFullItemParams(
+          name: testItemEntity.name,
+          description: testItemEntity.description,
+          categories: testItemEntity.categoryList,
+          people: testItemEntity.personList));
+
+      verify(repository.createFullItem(
+              name: testItemEntity.name,
+              description: testItemEntity.description,
+              categoryEntityList: testItemEntity.categoryList,
+              personEntityList: testItemEntity.personList))
+          .called(1);
+      expect(result, equals(Right(testItemEntity.id)));
+    });
+
+    test('should return DB Failure', () async {
+      CustomLogger.loggerNoStack.i('-> createFullItem() <- test starting...');
+
+      when(repository.createFullItem(
+              name: testItemEntity.name,
+              description: testItemEntity.description,
+              categoryEntityList: testItemEntity.categoryList,
+              personEntityList: testItemEntity.personList))
+          .thenAnswer(
+              (_) async => Left(DBFailure(errorMessage: DB_INSERT_FAILURE)));
+
+      final result = await usecase.call(CreateFullItemParams(
+          name: testItemEntity.name,
+          description: testItemEntity.description,
+          categories: testItemEntity.categoryList,
+          people: testItemEntity.personList));
+
+      verify(repository.createFullItem(
+              name: testItemEntity.name,
+              description: testItemEntity.description,
+              categoryEntityList: testItemEntity.categoryList,
+              personEntityList: testItemEntity.personList))
+          .called(1);
+      expect(result, equals(Left(DBFailure(errorMessage: DB_INSERT_FAILURE))));
+    });
+  });
+}
